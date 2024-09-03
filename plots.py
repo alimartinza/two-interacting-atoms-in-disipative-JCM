faf9d100613bbf52220da6b65da7abcf22b31353
Ali <ali.martin.zynda@gmail.com>
2024-09-03 13:46:30 -0300

    Logre hacer el plot 3D pero tarda mucho
    Descripcion detallada: Tarda mucho en levantar los datos, habria que
    hacer algo para que no tarde tanto, por ejemplo, subir las coherencias
    a un dataFrame y levantar todo de ahi.
    
    Modificado desde: [Notebook]
    
    Observaciones:

--- plots.py
+++ plots.py
@@ -19,8 +19,8 @@ p=0.005*g
 t_final=100000
 steps=100000
 t=np.linspace(0,t_final,steps)
-save_plot=True
-plot_show=False
+save_plot=False
+plot_show=True
 
 def plot_ReIm_coherencias(n:int,n_ax:int,xlabel=None,ylabel=None):
     '''
@@ -52,16 +52,16 @@ def plot_coherencias(n:int,n_ax:int,xlabel='gt',ylabel='Abs(Coh)'):
     i=0
     if n==1:
         for key in ['0,1','1,2','1,3','1,4','1,5','1,6','1,7','1,8','1,9','1,10','1,11']:
-            ax[n_ax].plot(g*t,np.abs(coherencias[key]),linestyle='dashed',color=colors[i],label=f'C({key})')
+            ax.plot(g*t,np.abs(coherencias[key]),linestyle='dashed',color=colors[i],label=f'C({key})')
             i+=1
     else:
         for key in coherencias.keys():
             if key.split(',')[0].startswith(str(n)) or key.split(',')[1].startswith(str(n)):
-                    ax[n_ax].plot(g*t,np.abs(coherencias[key]),linestyle='dashed',color=colors[i],label=f'C({key})')
+                    ax.plot(g*t,np.abs(coherencias[key]),linestyle='dashed',color=colors[i],label=f'C({key})')
                     i+=1
-    ax[n_ax].legend()
-    # ax[n_ax].set_xlabel(xlabel)
-    ax[n_ax].set_ylabel(ylabel)
+    # ax.legend()
+    # # ax[n_ax].set_xlabel(xlabel)
+    # ax[n_ax].set_ylabel(ylabel)
 
 for ci in condiciones_iniciales:
     for folder_names in folder_names:
@@ -79,17 +79,17 @@ for ci in condiciones_iniciales:
         gamma=[0.1*g,2*g]
 
 
-        for i,gamma in enumerate(gamma):
+        for m,gamma_m in enumerate(gamma):
             g_str=str(g).replace('.','_')
             k_str=str(k).replace('.','_')
             J_str=str(J).replace('.','_')
             d_str=str(d).replace('.','_')
             x_str=str(x).replace('.','_')
-            gamma_str=str(gamma).replace('.','_')
+            gamma_m_str=str(gamma_m).replace('.','_')
             p_str=str(p).replace('.','_')
             
-            param_name=f'g={g_str} k={k_str} J={J_str} d={d_str} x={x_str} gamma={gamma_str} p={p_str}'
-            csvname=f'g={g_str} k={k_str} J={J_str} d={d_str} x={x_str} gamma={gamma_str} p={p_str}.csv'
+            param_name=f'g={g_str} k={k_str} J={J_str} d={d_str} x={x_str} gamma={gamma_m_str} p={p_str}'
+            csvname=f'g={g_str} k={k_str} J={J_str} d={d_str} x={x_str} gamma={gamma_m_str} p={p_str}.csv'
             
             data=pd.read_csv(csvname)
             sol_states=fileio.qload(param_name+'sol states')
@@ -128,168 +128,172 @@ for ci in condiciones_iniciales:
             '''---------------PLOTS-----------------------'''
 
             '''--- N=0 ---'''
-            fig,ax=plt.subplots(1,1,figsize=(16, 9)) 
-            ax=[ax]
-            fig.suptitle('N=0')
-            ax[0].plot(g*t,data['pr(gg0)'],label=data.keys()[1],color='black')
-            plot_coherencias(9,0) #N=0
-            ax[0].set_xlabel('gt')
+            fig0 = plt.figure(figsize=(16,9))
+            ax0 = fig0.add_subplot(projection='3d')
+            colors=['red','black']
+            ax0.set_yticks(np.array(gamma)/g)
+            ax0.plot(g*t, data['pr(gg0)'], zs=gamma_m/g, zdir='y', color=colors[m], alpha=0.8)
+            fig0.suptitle('N=0')
+            plot_coherencias(9)#,0) #N=0
+            ax0.set_xlabel('gt')
+            ax0.set_ylabel('gamma/g')
+            ax0.set_zlabel('Amp. Prob. ')
+        if save_plot==True:
+            plt.savefig(f'0\\{param_name}',dpi=100)
+        else:
+            None
+        if plot_show==True:
+            plt.show()
+        else: 
+            None    
+        plt.close()
 
-            if save_plot==True:
-                plt.savefig(f'0\\{param_name}',dpi=100)
-            else:
-                None
-            if plot_show==True:
-                plt.show()
-            else: 
-                None    
-            plt.close()
-            '''--- N=1 ---'''
-            fig,ax=plt.subplots(3,1,figsize=(16, 9),sharex=True) 
-            fig.suptitle('N=1')
-            ax[0].plot(g*t,data['pr(gg1)'],label=data.keys()[1],color='black')
-            ax[0].plot(g*t,data['pr(eg0+ge0)'],label=data.keys()[2],color='blue')
-            ax[0].plot(g*t,data['pr(eg0-ge0)'],label=data.keys()[3],color='red')
-            plot_coherencias(3,0) #N=1
-            ax[1].plot(g*t,data['pr(gg1)'],label=data.keys()[1],color='black')
-            ax[1].plot(g*t,data['pr(eg0+ge0)'],label=data.keys()[2],color='blue')
-            ax[1].plot(g*t,data['pr(eg0-ge0)'],label=data.keys()[3],color='red')
-            plot_coherencias(4,1) #N=1
-            ax[2].plot(g*t,data['pr(gg1)'],label=data.keys()[1],color='black')
-            ax[2].plot(g*t,data['pr(eg0+ge0)'],label=data.keys()[2],color='blue')
-            ax[2].plot(g*t,data['pr(eg0-ge0)'],label=data.keys()[3],color='red')
-            ax[2].set_xlabel('gt')
-            plot_coherencias(10,2) #N=1
-            if plot_show==True:
-                plt.show()
-            else: 
-                None
-            if save_plot==True:
-                plt.savefig(f'1\\{param_name}',dpi=100)
-            else: 
-                None
-            plt.close()
-            '''--- N=2 ---'''
-            fig,ax=plt.subplots(2,2,figsize=(16, 9),tight_layout=True,sharex=True) 
-            ax=[ax[0][0],ax[0][1],ax[1][0],ax[1][1]]
-            fig.suptitle('N=2')
-            ax[0].plot(g*t,data['pr(gg2)'],label=data.keys()[4],color='black')
-            ax[0].plot(g*t,data['pr(eg1+ge1)'],label=data.keys()[5],color='blue')
-            ax[0].plot(g*t,data['pr(eg1-ge1)'],label=data.keys()[6],color='red')
-            ax[0].plot(g*t,data['pr(ee0)'],label=data.keys()[7],color='green')
-            plot_coherencias(0,0) #N=2
+'''--- N=1 ---'''
+fig,ax=plt.subplots(3,1,figsize=(16, 9),sharex=True) 
+fig.suptitle('N=1')
+ax[0].plot(g*t,data['pr(gg1)'],label=data.keys()[1],color='black')
+ax[0].plot(g*t,data['pr(eg0+ge0)'],label=data.keys()[2],color='blue')
+ax[0].plot(g*t,data['pr(eg0-ge0)'],label=data.keys()[3],color='red')
+plot_coherencias(3,0) #N=1
+ax[1].plot(g*t,data['pr(gg1)'],label=data.keys()[1],color='black')
+ax[1].plot(g*t,data['pr(eg0+ge0)'],label=data.keys()[2],color='blue')
+ax[1].plot(g*t,data['pr(eg0-ge0)'],label=data.keys()[3],color='red')
+plot_coherencias(4,1) #N=1
+ax[2].plot(g*t,data['pr(gg1)'],label=data.keys()[1],color='black')
+ax[2].plot(g*t,data['pr(eg0+ge0)'],label=data.keys()[2],color='blue')
+ax[2].plot(g*t,data['pr(eg0-ge0)'],label=data.keys()[3],color='red')
+ax[2].set_xlabel('gt')
+plot_coherencias(10,2) #N=1
+if plot_show==True:
+    plt.show()
+else: 
+    None
+if save_plot==True:
+    plt.savefig(f'1\\{param_name}',dpi=100)
+else: 
+    None
+plt.close()
+'''--- N=2 ---'''
+fig,ax=plt.subplots(2,2,figsize=(16, 9),tight_layout=True,sharex=True) 
+ax=[ax[0][0],ax[0][1],ax[1][0],ax[1][1]]
+fig.suptitle('N=2')
+ax[0].plot(g*t,data['pr(gg2)'],label=data.keys()[4],color='black')
+ax[0].plot(g*t,data['pr(eg1+ge1)'],label=data.keys()[5],color='blue')
+ax[0].plot(g*t,data['pr(eg1-ge1)'],label=data.keys()[6],color='red')
+ax[0].plot(g*t,data['pr(ee0)'],label=data.keys()[7],color='green')
+plot_coherencias(0,0) #N=2
 
-            ax[1].plot(g*t,data['pr(gg2)'],label=data.keys()[4],color='black')
-            ax[1].plot(g*t,data['pr(eg1+ge1)'],label=data.keys()[5],color='blue')
-            ax[1].plot(g*t,data['pr(eg1-ge1)'],label=data.keys()[6],color='red')
-            ax[1].plot(g*t,data['pr(ee0)'],label=data.keys()[7],color='green')
-            plot_coherencias(5,1) #N=2
+ax[1].plot(g*t,data['pr(gg2)'],label=data.keys()[4],color='black')
+ax[1].plot(g*t,data['pr(eg1+ge1)'],label=data.keys()[5],color='blue')
+ax[1].plot(g*t,data['pr(eg1-ge1)'],label=data.keys()[6],color='red')
+ax[1].plot(g*t,data['pr(ee0)'],label=data.keys()[7],color='green')
+plot_coherencias(5,1) #N=2
 
-            ax[2].plot(g*t,data['pr(gg2)'],label=data.keys()[4],color='black')
-            ax[2].plot(g*t,data['pr(eg1+ge1)'],label=data.keys()[5],color='blue')
-            ax[2].plot(g*t,data['pr(eg1-ge1)'],label=data.keys()[6],color='red')
-            ax[2].plot(g*t,data['pr(ee0)'],label=data.keys()[7],color='green')
-            ax[2].set_xlabel('gt')
-            plot_coherencias(6,2) #N=2 ESTA TIENE ALGUN PROBLEMA, SE GRAFICAN EL C(6,9) Y c(6,3) (CREO QUE ESOS) PERO DEBERIAN SER 0, Y SE GRAFICAN MUCHO NO ES ERROR NUMERICO
+ax[2].plot(g*t,data['pr(gg2)'],label=data.keys()[4],color='black')
+ax[2].plot(g*t,data['pr(eg1+ge1)'],label=data.keys()[5],color='blue')
+ax[2].plot(g*t,data['pr(eg1-ge1)'],label=data.keys()[6],color='red')
+ax[2].plot(g*t,data['pr(ee0)'],label=data.keys()[7],color='green')
+ax[2].set_xlabel('gt')
+plot_coherencias(6,2) #N=2 ESTA TIENE ALGUN PROBLEMA, SE GRAFICAN EL C(6,9) Y c(6,3) (CREO QUE ESOS) PERO DEBERIAN SER 0, Y SE GRAFICAN MUCHO NO ES ERROR NUMERICO
 
-            ax[3].plot(g*t,data['pr(gg2)'],label=data.keys()[4],color='black')
-            ax[3].plot(g*t,data['pr(eg1+ge1)'],label=data.keys()[5],color='blue')
-            ax[3].plot(g*t,data['pr(eg1-ge1)'],label=data.keys()[6],color='red')
-            ax[3].plot(g*t,data['pr(ee0)'],label=data.keys()[7],color='green')
-            ax[3].set_xlabel('gt')
-            plot_coherencias(11,3) #N=2
-            if plot_show==True:
-                plt.show()
-            else: 
-                None
-            if save_plot==True:
-                plt.savefig(f'2\\{param_name}',dpi=100)
-            else: 
-                None
-            plt.close()
-            '''--- N=3 ---'''
+ax[3].plot(g*t,data['pr(gg2)'],label=data.keys()[4],color='black')
+ax[3].plot(g*t,data['pr(eg1+ge1)'],label=data.keys()[5],color='blue')
+ax[3].plot(g*t,data['pr(eg1-ge1)'],label=data.keys()[6],color='red')
+ax[3].plot(g*t,data['pr(ee0)'],label=data.keys()[7],color='green')
+ax[3].set_xlabel('gt')
+plot_coherencias(11,3) #N=2
+if plot_show==True:
+    plt.show()
+else: 
+    None
+if save_plot==True:
+    plt.savefig(f'2\\{param_name}',dpi=100)
+else: 
+    None
+plt.close()
+'''--- N=3 ---'''
 
-            fig,ax=plt.subplots(1,1,figsize=(16, 9)) 
-            ax=[ax]
-            fig.suptitle('N=3')
-            ax[0].plot(g*t,data['pr(eg2)'],label=data.keys()[8],color='black')
-            ax[0].plot(g*t,data['pr(ge2)'],label=data.keys()[9],color='blue')
-            ax[0].plot(g*t,data['pr(ee1)'],label=data.keys()[10],color='red')
-            plot_coherencias(1,0) #N=3
-            plot_coherencias(7,0) #N=3
-            plot_coherencias(8,0) #N=3
-            if plot_show==True:
-                plt.show()
-            else: 
-                None
-            if save_plot==True:
-                plt.savefig(f'3\\{param_name}',dpi=100)
-            else: 
-                None
-            plt.close()
-            '''--- VM Pauli ---'''
-            fig,ax=plt.subplots(1,1,figsize=(16, 9))
-            fig.suptitle('V.M. Pauli')
-            plt.plot(g*t,data['1/2 <sz1+sz2>'],label=data.keys()[11],color='black')
-            plt.plot(g*t,data['<sx1>'],label=data.keys()[12],color='blue')
-            plt.plot(g*t,data['<sx2>'],label=data.keys()[13],color='red')
-            plt.legend()
-            if plot_show==True:
-                plt.show()
-            else: 
-                None
-            if save_plot==True:
-                plt.savefig(f'pauli\\{param_name}',dpi=100)
-            else: 
-                None
-            plt.close()
+fig,ax=plt.subplots(1,1,figsize=(16, 9)) 
+ax=[ax]
+fig.suptitle('N=3')
+ax[0].plot(g*t,data['pr(eg2)'],label=data.keys()[8],color='black')
+ax[0].plot(g*t,data['pr(ge2)'],label=data.keys()[9],color='blue')
+ax[0].plot(g*t,data['pr(ee1)'],label=data.keys()[10],color='red')
+plot_coherencias(1,0) #N=3
+plot_coherencias(7,0) #N=3
+plot_coherencias(8,0) #N=3
+if plot_show==True:
+    plt.show()
+else: 
+    None
+if save_plot==True:
+    plt.savefig(f'3\\{param_name}',dpi=100)
+else: 
+    None
+plt.close()
+'''--- VM Pauli ---'''
+fig,ax=plt.subplots(1,1,figsize=(16, 9))
+fig.suptitle('V.M. Pauli')
+plt.plot(g*t,data['1/2 <sz1+sz2>'],label=data.keys()[11],color='black')
+plt.plot(g*t,data['<sx1>'],label=data.keys()[12],color='blue')
+plt.plot(g*t,data['<sx2>'],label=data.keys()[13],color='red')
+plt.legend()
+if plot_show==True:
+    plt.show()
+else: 
+    None
+if save_plot==True:
+    plt.savefig(f'pauli\\{param_name}',dpi=100)
+else: 
+    None
+plt.close()
 
-            '''--- Entropias ---'''
-            #PLOT PARA LAS ENTROPIAS
-            fig,ax=plt.subplots(2,1,figsize=(16, 9),sharex=True)
-            fig.suptitle("Entropia en A-A-F")
-            ax[0].plot(g*t,data['S von Neuman tot'],color='black')
-            # ax[0].set_xlabel('t')
-            ax[0].set_ylabel('S_vn')
+'''--- Entropias ---'''
+#PLOT PARA LAS ENTROPIAS
+fig,ax=plt.subplots(2,1,figsize=(16, 9),sharex=True)
+fig.suptitle("Entropia en A-A-F")
+ax[0].plot(g*t,data['S von Neuman tot'],color='black')
+# ax[0].set_xlabel('t')
+ax[0].set_ylabel('S_vn')
 
-            ax[1].plot(g*t,data['S lineal tot'],color='red')
-            ax[1].set_xlabel('t')
-            ax[1].set_ylabel('S_lin')
-            if plot_show==True:
-                plt.show()
-            else: 
-                None
-            if save_plot==True:
-                plt.savefig(f'entropia\\{param_name}',dpi=100)
-            else: 
-                None
-            plt.close()
-            #PLOT PARA LA DISTRIBUCION DE WIGNER. QUIZAS HACER UNA SIMULACION ESTARIA COPADO
+ax[1].plot(g*t,data['S lineal tot'],color='red')
+ax[1].set_xlabel('t')
+ax[1].set_ylabel('S_lin')
+if plot_show==True:
+    plt.show()
+else: 
+    None
+if save_plot==True:
+    plt.savefig(f'entropia\\{param_name}',dpi=100)
+else: 
+    None
+plt.close()
+#PLOT PARA LA DISTRIBUCION DE WIGNER. QUIZAS HACER UNA SIMULACION ESTARIA COPADO
 
-            '''---Trazamos sobre el campo---'''
-            #Y TOMANDO TRAZA PARCIAL SOBRE EL CAMPO, MIRAMOS EL ENTRELAZAMIENTO ENTRE ATOMOS
+'''---Trazamos sobre el campo---'''
+#Y TOMANDO TRAZA PARCIAL SOBRE EL CAMPO, MIRAMOS EL ENTRELAZAMIENTO ENTRE ATOMOS
 
 
-            #PLOT PARA LAS ENTROPIAS DEL SISTEMA TRAZANDO SOBRE LOS FOTONES
-            fig,ax=plt.subplots(3,1,figsize=(16, 9),sharex=True)
-            fig.suptitle("Sist. A-A sin foton")
-            ax[0].plot(g*t,data['S vN atom'],color='black')
-            # ax[0].set_xlabel('t')
-            ax[0].set_ylabel('S_vn')
+#PLOT PARA LAS ENTROPIAS DEL SISTEMA TRAZANDO SOBRE LOS FOTONES
+fig,ax=plt.subplots(3,1,figsize=(16, 9),sharex=True)
+fig.suptitle("Sist. A-A sin foton")
+ax[0].plot(g*t,data['S vN atom'],color='black')
+# ax[0].set_xlabel('t')
+ax[0].set_ylabel('S_vn')
 
-            ax[1].plot(g*t,data['S lin atom'],color='red')
-            # ax[1].set_xlabel('t')
-            ax[1].set_ylabel('S_lin')
+ax[1].plot(g*t,data['S lin atom'],color='red')
+# ax[1].set_xlabel('t')
+ax[1].set_ylabel('S_lin')
 
-            ax[2].plot(g*t,data['Concu atom'],color='blue')
-            ax[2].set_xlabel('t')
-            ax[2].set_ylabel('Concurrence')
-            if plot_show==True:
-                plt.show()
-            else: 
-                None
-            if save_plot==True:
-                plt.savefig(f'entropia_spin-spin\\{param_name}',dpi=100)
-            else: 
-                None
-            plt.close()
+ax[2].plot(g*t,data['Concu atom'],color='blue')
+ax[2].set_xlabel('t')
+ax[2].set_ylabel('Concurrence')
+if plot_show==True:
+    plt.show()
+else: 
+    None
+if save_plot==True:
+    plt.savefig(f'entropia_spin-spin\\{param_name}',dpi=100)
+else: 
+    None
+plt.close()
