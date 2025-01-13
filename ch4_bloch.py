f325a7f00437631e9a7f14b88089c7281b193cbd
Ali <ali.martin.zynda@gmail.com>
2025-01-13 19:18:07 -0300

    backup

--- ch4_bloch.py
+++ ch4_bloch.py
@@ -105,8 +105,8 @@ k=0 # .1*g
 J=0
 
 
-psi0=(eg0+ge0).unit()  #gg1#(tensor(tensor(e,gr)+tensor(gr,gr),basis(3,0)+basis(3,1))).unit()#1/10*(gg0*gg0.dag()+(eg0+ge0).unit()*(eg0+ge0).unit().dag()+(eg0-ge0).unit()*(eg0-ge0).unit().dag()+gg1*gg1.dag()+ee0*ee0.dag()+(eg1+ge1).unit()*(eg1+ge1).unit().dag()+(eg1-ge1).unit()*(eg1-ge1).unit().dag()+gg2*gg2.dag()+(eg2+ge2).unit()*(eg2+ge2).unit().dag()+(eg2-ge2).unit()*(eg2-ge2).unit().dag())
-psi0Name='eg0+ge0'
+psi0=(eg0).unit()  #gg1#(tensor(tensor(e,gr)+tensor(gr,gr),basis(3,0)+basis(3,1))).unit()#1/10*(gg0*gg0.dag()+(eg0+ge0).unit()*(eg0+ge0).unit().dag()+(eg0-ge0).unit()*(eg0-ge0).unit().dag()+gg1*gg1.dag()+ee0*ee0.dag()+(eg1+ge1).unit()*(eg1+ge1).unit().dag()+(eg1-ge1).unit()*(eg1-ge1).unit().dag()+gg2*gg2.dag()+(eg2+ge2).unit()*(eg2+ge2).unit().dag()+(eg2-ge2).unit()*(eg2-ge2).unit().dag())
+psi0Name='eg0'
 # print(psi0)
 steps=20000
 esfera02=Bloch()
@@ -118,7 +118,7 @@ colors_pob=mpl.colormaps['plasma'](np.linspace(0,1,4))
 fig_pob_B=plt.figure(figsize=(8,6))
 ax_B=fig_pob_B.add_subplot()
 
-for l,d in enumerate([0,0.5*g,2*g]):
+for l,x in enumerate([0,0.5*g,2*g]):
     T=2*np.pi/omega_general(1,1,d,g,k,J,x) 
 
     t_final=1*T/np.sqrt(2)
--- plots.py
+++ plots.py
@@ -42,21 +42,57 @@ gg1=tensor(gr,gr,basis(3,1)) #10
 gg2=tensor(gr,gr,basis(3,2)) #11
 
 
+def beta_n(n_:int,k:float,J:float,x:float):
+    return -(x*(n_**2+(n_-1)**2+(n_-2)**2)+J+2*k)
+
+def gamma_n(n_:int,d:float,g:float,k:float,J:float,x:float,a:float=0.5):
+    return (x*(n_-1)**2-J+2*k)*(x*(n_-2)**2+x*n_**2+2*J)+(x*(n_-2)**2+d+J)*(x*n_**2-d+J)-2*g**2*(n_**(2*a)+(n_-1)**(2*a))
+
+def eta_n(n_:int,d:float,g:float,k:float,J:float,x:float,a:float=0.5):
+    return -(x*n_**2 - d + J)*(x*(n_ - 2)**2 + d + J)*(x*(n_ - 1)**2 - J + 2*k)+ 2*g**2*(x*(n_ - 2)**2*n_**(2*a) + x*n_**2*(n_ - 1)**(2*a) + d* (n_**(2*a) - (n_ - 1)**(2*a)) + J*(n_**(2*a) - (n_ - 1)**(2*a)))
+
+def Q_n(n_:int,d:float,g:float,k:float,J:float,x:float):
+    return gamma_n(n_,d,g,k,J,x)/3-beta_n(n_,k,J,x)*beta_n(n_,k,J,x)/9
+
+def R_n(n_:int,d:float,g:float,k:float,J:float,x:float):
+    return 1/54*(9*beta_n(n_,k,J,x)*gamma_n(n_,d,g,k,J,x)-27*eta_n(n_,d,g,k,J,x)-2*beta_n(n_,k,J,x)*beta_n(n_,k,J,x)*beta_n(n_,k,J,x))
+
+def theta_n(n_:int,d:float,g:float,k:float,J:float,x:float):
+    return np.arccos(R_n(n_,d,g,k,J,x)/np.sqrt(-Q_n(n_,d,g,k,J,x)**3))
+
+def omega_general(n_:int,j:int,d:float,g:float,k:float,J:float,x:float):
+    return 2*np.sqrt(-2*Q_n(n_,d,g,k,J,x))*np.cos((theta_n(n_,d,g,k,J,x)+2*(j-1)*np.pi)/3)
+
+def energiasn1(j,g,d,x,k,J):
+    if j==1: return (x-d)/2+k+np.sqrt(2*g**2+(k-J+(d-x)/2)**2)
+    elif j==2: return (x-d)/2+k-np.sqrt(2*g**2+(k-J+(d-x)/2)**2)
+    elif j==3: return -2*k-J
+    else: 
+        print('valor inesperado de j')
+        exit()
+
 w0=1
-J=0
-x=0
 g=0.001*w0
-k=0
+
 p=0.005*g
 gamma=0.1*g
-alpha=0
 
-psi0=(ee0).unit()  #gg1#(tensor(tensor(e,gr)+tensor(gr,gr),basis(3,0)+basis(3,1))).unit()#1/10*(gg0*gg0.dag()+(eg0+ge0).unit()*(eg0+ge0).unit().dag()+(eg0-ge0).unit()*(eg0-ge0).unit().dag()+gg1*gg1.dag()+ee0*ee0.dag()+(eg1+ge1).unit()*(eg1+ge1).unit().dag()+(eg1-ge1).unit()*(eg1-ge1).unit().dag()+gg2*gg2.dag()+(eg2+ge2).unit()*(eg2+ge2).unit().dag()+(eg2-ge2).unit()*(eg2-ge2).unit().dag())
+d=0
+x=0
+
+J=0
+k=0
+
+alpha=1
+
+psi0=(gg1).unit()  #gg1#(tensor(tensor(e,gr)+tensor(gr,gr),basis(3,0)+basis(3,1))).unit()#1/10*(gg0*gg0.dag()+(eg0+ge0).unit()*(eg0+ge0).unit().dag()+(eg0-ge0).unit()*(eg0-ge0).unit().dag()+gg1*gg1.dag()+ee0*ee0.dag()+(eg1+ge1).unit()*(eg1+ge1).unit().dag()+(eg1-ge1).unit()*(eg1-ge1).unit().dag()+gg2*gg2.dag()+(eg2+ge2).unit()*(eg2+ge2).unit().dag()+(eg2-ge2).unit()*(eg2-ge2).unit().dag())
 # print(psi0)
 steps=10000
-t_final=10*steps
+T=2*np.pi/omega_general(1,1,d,g,k,J,x)
+
+t_final=2*T
 
 
 # jcm_lib.plot_gamma_simu(w0,delta=0,chi=0,g=g,k=0,J=0,gamma_list=[0,0.1*g,0.5*g,g,2*g],alpha=alpha,psi0=psi0,disipation=True,steps=steps,t_final=t_final)
 
-jcm_lib.plot_gamma_simu(w0,delta=2*g,chi=0,g=g,k=0,J=0,gamma_list=[0.1*g],alpha=alpha,psi0=psi0,disipation=False,steps=steps,t_final=t_final)
+jcm_lib.plot_gamma_simu(w0,delta=d,chi=x,g=g,k=k,J=J,gamma_list=[0,0.1*g,0.5*g,2*g],alpha=alpha,psi0=psi0,disipation=True,steps=steps,t_final=t_final)
